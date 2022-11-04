e1571d34ffbbff3f14569acf9058410804f4ef76
salkom <salmonbek37@mail.ru>
2022-11-04 23:39:01 +0500

    Add project files.

---
+++ ParseCSV/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParseCSV
+{
+    public class Player
+    {
+        public int Rank { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Country { get; set; } 
+        public int Rating { get; set; } 
+        public int Games { get; set; }
+        public int BYear { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format($"Rank: {Rank}\nName: {Name}\nTitle: {Title}\nCountry: {Country}\nRating: {Rating}\nGames: {Games}\nB-Year: {BYear}\n");
+        }
+    }
+}
---
+++ ParseCSV/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ParseCSV
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"Top100ChessPlayers.csv";
+            var playersList = new List<Player>();
+
+            if (File.Exists(path))
+            {
+                using (StreamReader file = new StreamReader(path))
+                {
+                    int index = 0;
+                    string ln;
+
+                    string[] parts;
+
+                    while ((ln = file.ReadLine()) != null)
+                    {
+                        parts = ln.Split(';');
+                        index++;
+
+                        if (index <= 1) continue;
+
+                        playersList.Add(new Player()
+                        {
+                            Rank = int.Parse(parts[0]),
+                            Name = parts[1],
+                            Title = parts[2],
+                            Country = parts[3],
+                            Rating = int.Parse(parts[4]),
+                            Games = int.Parse(parts[5]),
+                            BYear = int.Parse(parts[6])
+                        });
+                    }
+
+                    file.Close();  
+                }
+            }
+
+            IEnumerable<Player> query = playersList
+                .Where(p => p.BYear > 1980).Take(10);
+
+            foreach (var player in query)
+            {
+                Console.WriteLine(player);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
